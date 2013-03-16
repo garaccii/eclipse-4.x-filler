2c6ca75c651f616f4143f1343b730d2f66299b93
atlanto <atlanto@github.com>
2013-03-16 18:55:39 +0900

    bugfix - not working at initial time

--- pdt_tools.HideQuickAccess/src/jp/sourceforge/pdt_tools/hidequickaccess/Startup.java
+++ pdt_tools.HideQuickAccess/src/jp/sourceforge/pdt_tools/hidequickaccess/Startup.java
@@ -1,14 +1,30 @@
 package jp.sourceforge.pdt_tools.hidequickaccess;
 
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.ui.IStartup;
 import org.eclipse.ui.IWindowListener;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.progress.UIJob;
 
 public class Startup implements IStartup, IWindowListener {
 
 	@Override
 	public void earlyStartup() {
+		new UIJob("") {
+			@Override
+			public IStatus runInUIThread(IProgressMonitor monitor) {
+				IWorkbenchWindow window = PlatformUI.getWorkbench()
+						.getActiveWorkbenchWindow();
+				if (window != null) {
+					boolean state = HideQuickAccess.getCommandState(window);
+					HideQuickAccess.hideQuickAccess(window, !state);
+				}
+				return Status.OK_STATUS;
+			}
+		}.schedule();
 		PlatformUI.getWorkbench().addWindowListener(this);
 	}
 
